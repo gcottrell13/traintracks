b69fcb0977b1188d3322ad4bade10e49e4f7f8b5
Gage <inquisitorpinkie@gmail.com>
2025-03-21 15:42:55 -0700

    curved rail

--- MaterialCache.cs
+++ MaterialCache.cs
@@ -5,23 +5,38 @@ namespace traintracks;
 
 public static class MaterialCache
 {
-    private static Dictionary<string, StandardMaterial3D> Cache = new();
-    public static StandardMaterial3D? GetMaterial(Texture2D? texture)
+    private readonly static Dictionary<string, StandardMaterial3D> StandardMaterial3DCache = [];
+    public static StandardMaterial3D GetMaterial(Texture2D texture)
     {
-        if (texture == null)
-            return null;
         var path = texture.ResourcePath;
-        if (!Cache.TryGetValue(path, out var material))
+        if (!StandardMaterial3DCache.TryGetValue(path, out var material))
         {
-            material = new StandardMaterial3D();
-            material.AlbedoTexture = texture;
-            Cache[path] = material;
+            material = new StandardMaterial3D
+            {
+                AlbedoTexture = texture
+            };
+            StandardMaterial3DCache[path] = material;
         }
         return material;
     }
 
+    private static ShaderMaterial SetShaderParams(ShaderMaterial shader, Dictionary<string, Variant> parameters)
+    {
+        foreach (var key in parameters.Keys)
+            shader.SetShaderParameter(key, parameters[key]);
+        return shader;
+    }
+
     public static class Images
     {
-        public static StandardMaterial3D? TrainTrackUV1 => GetMaterial(ResourceLoader.Load("res://images/traintrack-uv1.png") as Texture2D);
+        public readonly static Material TrainTrackUV1 = GetMaterial(ResourceLoader.Load<Texture2D>("res://images/traintrack-uv1.png"));
+        public readonly static ShaderMaterial TrainTrackUV2 = SetShaderParams(
+            new ShaderMaterial() { Shader = ResourceLoader.Load<Shader>("res://models/doubleRail.gdshader") },
+            new() { 
+                { "texture_albedo", ResourceLoader.Load<Texture2D>("res://images/traintrack-uv2.png") } ,
+                { "albedo", Vector4.One },
+                { "uv1_scale", Vector3.One },
+            }
+            );
     }
 }
--- TrackStraight.cs
+++ TrackStraight.cs
@@ -4,32 +4,34 @@ namespace traintracks;
 
 public enum TrackType
 {
-	OldRail = 1,
+	MonoRail = 1,
+	DoubleRail = 2,
 }
 
 [Tool]
 public partial class TrackStraight : Node3D
 {
 
-	[Export]
-	public TrackType TrackType { get; set; } = TrackType.OldRail;
+	private TrackType _trackType = TrackType.DoubleRail;
 
 	[Export]
-	public Curve3D? Curve { get; set; }
+	public TrackType TrackType { get => _trackType; set { _trackType = value; GenerateRailGeometry(); } }
 
-	private Node3D? trackModel => (Node3D)FindChild("track");
+	private float? _lastCurveLength;
+	private Curve3D? _curve;
+	[Export]
+	public Curve3D? Curve { get => _curve; set { SetCurve(value); } }
 
-	public void GenerateRailGeometry()
-	{
-		switch (TrackType)
-		{
-			case TrackType.OldRail:
-				{
-					_oldRailGeometry();
-					break;
-				}
-		}
-	}
+	private Node3D? TrackModel => (Node3D)FindChild("track");
+
+	public const float DoubleRailSize = 0.2f;
+	public const float DoubleRailBarSize = 0.2f;
+	public const float DoubleRailClip = 0.05f;
+
+	public const int SubdivideDepth = 3;
+
+	public const float MinZ = -4;
+	public const float MaxZ = 5;
 
 	public override void _Ready()
 	{
@@ -37,18 +39,147 @@ public partial class TrackStraight : Node3D
 		GenerateRailGeometry();
 	}
 
-	private void _oldRailGeometry()
+	public override void _Process(double delta)
 	{
-		if (trackModel == null)
-			return;
-		foreach (var child in trackModel.GetChildren())
-			trackModel.RemoveChild(child);
+		base._Process(delta);
 
-		var boxMesh = new BoxMesh() {
-			SubdivideDepth = 3,
+		var newCurve = Curve?.GetBakedLength();
+
+		if (newCurve != _lastCurveLength)
+		{
+			SetCurve(Curve);
+			_lastCurveLength = newCurve;
+		}
+	}
+
+	public void SetCurve(Curve3D? curve)
+	{
+		_curve = curve;
+		switch (TrackType)
+		{
+			case TrackType.DoubleRail:
+				{
+					UpdateDoubleRailCurve();
+					break;
+				}
+		}
+	}
+
+	public void GenerateRailGeometry()
+	{
+		switch (TrackType)
+		{
+			case TrackType.DoubleRail:
+				{
+					DoubleRailGeometry();
+					break;
+				}
+		}
+	}
+
+	private void DoubleRailGeometry()
+	{
+		if (TrackModel == null)
+			return;
+		foreach (var child in TrackModel.GetChildren())
+			TrackModel.RemoveChild(child);
+
+		for (var i = MinZ; i <= MaxZ; i++)
+		{
+			var part = CreateDoubleRailTrackPart();
+			TrackModel.AddChild(part);
+			part.TargetZ = i;
+		}
+	}
+
+	private void UpdateDoubleRailCurve()
+	{
+		if (TrackModel == null)
+			return;
+
+		foreach (var child in TrackModel.GetChildren())
+		{
+			if (child is not DoubleRailTrackPart part)
+				continue;
+			//var step = 1.0f / SubdivideDepth;
+
+			//for (var f = -0.5f; f <= 0.5f; f += step)
+			//{
+
+			//}
+			if (Curve == null)
+			{
+				part.SetDoubleTrailTransform3D(Transform3D.Identity);
+			}
+			else
+			{
+				var s = (part.TargetZ - MinZ) / (MaxZ - MinZ);
+				var len = Curve.GetBakedLength();
+				part.SetDoubleTrailTransform3D(Curve.SampleBakedWithRotation(s * len, applyTilt: true));
+			}
+				
+		}
+	}
+
+	private static DoubleRailTrackPart CreateDoubleRailTrackPart()
+	{
+		var barMesh = new MeshInstance3D
+		{
+			Name = "BarMesh",
+			Mesh = new BoxMesh()
+			{
+				Size = new(2.5f, DoubleRailBarSize, DoubleRailBarSize),
+				Material = MaterialCache.Images.TrainTrackUV1,
+			},
+			Position = new(0, DoubleRailBarSize / 2 - 0.05f, 0)
 		};
-		var box = new MeshInstance3D() { Mesh = boxMesh };
-		trackModel.AddChild(box);
-		box.SetSurfaceOverrideMaterial(0, MaterialCache.Images.TrainTrackUV1);
+		var leftRailMesh = new MeshInstance3D()
+		{
+			Name = "LeftRailMesh",
+			Mesh = new BoxMesh()
+			{
+				SubdivideDepth = SubdivideDepth,
+				Size = new(DoubleRailSize, DoubleRailSize, 1),
+				Material = MaterialCache.Images.TrainTrackUV2,
+			}, 
+			Position = new(1, barMesh.Position.Y + DoubleRailSize - DoubleRailClip, 0),
+		};
+		var rightRailMesh = new MeshInstance3D
+		{
+			Name = "RightRailMesh",
+			Mesh = new BoxMesh()
+			{
+				SubdivideDepth = SubdivideDepth,
+				Size = new(DoubleRailSize, DoubleRailSize, 1),
+				Material = MaterialCache.Images.TrainTrackUV2,
+			},
+			Position = new(-1, leftRailMesh.Position.Y, 0)
+		};
+		return new DoubleRailTrackPart().SetMeshes(leftRailMesh, rightRailMesh, barMesh);
+	}
+
+	private partial class DoubleRailTrackPart : Node3D
+	{
+		public MeshInstance3D? leftRailMesh;
+		public MeshInstance3D? rightRailMesh;
+		public MeshInstance3D? barMesh;
+		public float TargetZ;
+
+		public DoubleRailTrackPart SetMeshes(MeshInstance3D left, MeshInstance3D right, MeshInstance3D bar)
+		{
+			leftRailMesh = left;
+			rightRailMesh = right;
+			barMesh = bar;
+			AddChild(leftRailMesh);
+			AddChild(rightRailMesh);
+			AddChild(barMesh);
+			return this;
+		}
+
+		public void SetDoubleTrailTransform3D(Transform3D transform)
+		{
+			Transform = transform;
+
+		}
 	}
 }
